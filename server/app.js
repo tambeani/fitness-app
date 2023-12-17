0385302d6ad2b6a28c107ba76b2a3df5085f0d31
Raveena Ingale <ingale.r@northeastern.edu>
2023-12-16 20:21:36 -0500

    Started express backend

---
+++ server/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express(); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req,res)=>{
+    res.send("Hello world!");
+});
+
+app.listen(3000,()=>{
+    console.log("App started listening on port 3000")
+});
